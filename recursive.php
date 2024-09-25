470f83907f0a621d55b5239cf778e9bcccaf25f8
AlyssaTifara <tissaalyssa@gmail.com>
2024-09-25 13:13:50 +0700

    pk5

--- recursive.php
+++ recursive.php
@@ -1,9 +1,10 @@
 <?php
-function tampilkanAngka(int $jumlah, int $indeks = 1){
+
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
     echo "Perulangan ke-{$indeks} <br>";
-    
-    for ($i=1; $i<=25 ; $i++) {
-        echo "Perulangan ke-{$i} <br>";
+
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
     }
 }
 tampilkanAngka(20);
