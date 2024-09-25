c9764a1396658510f0c16357ec2b308136721a1f
AlyssaTifara <tissaalyssa@gmail.com>
2024-09-25 13:32:35 +0700

    tugas

--- multi_menu.php
+++ multi_menu.php
@@ -10,32 +10,40 @@ $menu = [
                 "nama" => "Wisata",
                 "subMenu" => [
                     [
+                        "nama" => "Pantai"
+                    ],
+                    [
                         "nama" => "Gunung"
                     ]
                 ]
-                    ],
-                    [
-                        "nama" => "Kuliner"
-                    ],
-                    [
-                        "nama" => "Hiburan"
-                    ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
         ]
-                    ],
-                    [
-                        "nama" => "Tentang"
-                    ],
-                    [
-                        "nama" => "Kontak"
-                    ],
-                ];
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
 
-                function tampilkanMenuBertingkat (array $menu) {
-                    echo "<ul>";
-                    foreach ($menu as $key => $item) {
-                        echo "<li>{$item['nama']}</li>";
-                    }
-                    echo "</ul";
-                }
-                tampilkanMenuBertingkat($menu);
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}";
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+        echo "</li>";
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
 ?>
