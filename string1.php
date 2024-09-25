3ed66aa9961bb0efc23799e350574e082b92f725
AlyssaTifara <tissaalyssa@gmail.com>
2024-09-25 14:12:26 +0700

    melarikanDiri

---
+++ string1.php
@@ -0,0 +1,20 @@
+<?php
+// $loremIpsum = "Lorem ipsum dolor sit amet consectuer adipsinting elit. Lorem ipsum dolor sit amet consectuer adipsinting elit. Lorem ipsum dolor sit amet consectuer adipsinting elit.Lorem ipsum dolor sit amet consectuer adipsinting elit.";
+
+// echo "<p>{$loremIpsum}</p>";
+// echo "Panjang karakter: " . strlen($loremIpsum) . "<br>";
+// echo "Panjang kata: " . str_word_count($loremIpsum) . "<br>";
+// echo "<p>" . strtoupper($loremIpsum) . "</p>";
+// echo "<p>" . strtolower($loremIpsum) . "</p>";
+
+echo "Baris\nbaru <br>";
+echo 'Baris\nbaru <br>';
+echo "Halo\nDunia <br>";
+echo 'Halo\nDunia <br>';
+
+echo "<pre>Hallo\tDunia!</pre>";
+echo '<pre>Hallo\tDunia!</pre>';
+
+echo "Katakanlah \"Tidak pada narkoba!\" <br>";
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>';
+?>
